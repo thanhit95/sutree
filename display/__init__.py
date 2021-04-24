209fc59943c79bd1b5b288962717c47b25cf81c5
thanhit95 <thanh.it1995@gmail.com>
2021-04-24 09:49:55 +0700

    add docstring for package display

--- display/__init__.py
+++ display/__init__.py
@@ -1,3 +1,17 @@
+'''
+
+TREE DISPLAY
+
+Description:    A utility help visualize trees by using ASCII text.
+
+Author:         Thanh Trung Nguyen
+                thanh.it1995 (at) gmail.com
+
+License:        3-Clause BSD License
+
+'''
+
+
 from .nonbintreedisplay import NonBinTreeDisplay
 
 __all__ = [
